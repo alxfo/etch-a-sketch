76922f6e372180ea3dc68680135b57eca2f7661d
alxfo <120241709+alxfo@users.noreply.github.com>
2023-01-23 15:21:21 +0800

    Add button to prompt user for grid size

--- javascript.js
+++ javascript.js
@@ -1,17 +1,42 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
+const btn = document.querySelector('#size');
+
+function changeSize() {
+    let size = prompt('How many squares per side? (max 100)');
+
+    totalPanels = size * size
+
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    
+    for (let i = 0; i < totalPanels; i++) {
+        let div = document.createElement('div');
+        div.classList.add('panel');
+        container.appendChild(div);
+    }
+
+    const panels = document.querySelectorAll('.panel');
+
+    for (let panel of panels) {
+        panel.addEventListener('mouseover', changeColour);
+    };
+};
+
+btn.addEventListener('click', changeSize);
 
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
-    div.classList.add('panel')
+    div.classList.add('panel');
     container.appendChild(div);
-}
+};
 
 function changeColour(e) {
-    e.srcElement.setAttribute('style', 'background: black;')
-}
+    e.srcElement.setAttribute('style', 'background: black;');
+};
 
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
 
 for (let panel of panels) {
-    panel.addEventListener('mouseover', changeColour)
-}
+    panel.addEventListener('mouseover', changeColour);
+};
